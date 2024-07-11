f0a80ae805fcbfbd4bb5d0683d5b7486cfcd8b4b
Ania <aniaa.pawlik@gmail.com>
2024-07-11 23:56:47 +0200

    task implementation

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,31 @@
+import React, {useState} from 'react';
+
 export const Counter = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  const handleOperation = (operation) => {
+    switch (operation) {
+      case "increment":
+        setCounter(counter + 1);
+        break;
+      case "decrement":
+        counter === 0 ? setCounter(0) : setCounter(counter - 1)
+        break;
+      case "reset":
+        setCounter(0);
+        break;
+      default:
+        break
+    }
+  };
+
   return (
     <div>
-      <h1>Counter: 0</h1>
-      <button>+</button>
-      <button>-</button>
+      <p>Counter: {counter}</p>
+      <button onClick={() => handleOperation("decrement")}>-1</button>
+      <button onClick={() => handleOperation("increment")}>+1</button>
+      <button onClick={() => handleOperation("reset")}>Reset</button>
     </div>
-  );
-};
+  )
+}
