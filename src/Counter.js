dffb180e056f95dd666aaba133bcfabe41b561f2
Ania <aniaa.pawlik@gmail.com>
2024-07-24 00:03:30 +0200

    remove button reset and add input

--- src/Counter.js
+++ src/Counter.js
@@ -11,16 +11,17 @@ export const Counter = (props) => {
     props.value === 0 ? props.onChange(0) : props.onChange(props.value - 1)
   }
 
-  const handleReset = () => {
-    props.onChange(0);
+  const handleInputChange = (event) => {
+    const value = event.target.value.replace(/\+|-/ig, '');
+    value <= props.maxValue ? props.onChange(Number(value)) : props.onChange(props.maxValue)
   }
 
   return (
     <div>
       <p>{props.tag}: {props.value} </p>
+      <input type='text' value={props.value} onChange={handleInputChange} />
       <button onClick={handleDecrement} disabled={props.value === 0}>-1</button>
       <button onClick={handleIncrement} disabled={props.value === props.maxValue}>+1</button>
-      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
